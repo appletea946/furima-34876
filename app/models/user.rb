b0924c5395447e50780c0c2b051ab85636e151b8
appletea946 <shouyisheng@gmail.com>
2021-03-10 14:55:53 +0900

    全角漢字と全角カナのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,18 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_katakana, presence: true
-  validates :first_name_katakana, presence: true
   validates :birthday, presence: true
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: "should include both letters and numbers"
+  validates_format_of :password, with: PASSWORD_REGEX, message: "には英字と数字の両方を含めて設定してください"
+
+  with_options presence: true, format: { with: /\A[一-龥々]+\z/, message:'には、全角漢字を使用してください'} do
+    validates :last_name_kanji
+    validates :first_name_kanji
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message:'には、全角カタカナを使用してください'} do
+    validates :last_name_katakana
+    validates :first_name_katakana
+  end
 end
