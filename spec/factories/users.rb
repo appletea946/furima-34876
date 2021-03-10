a57ba92ed2104174d4ae4330d66badf3cd465ad3
appletea946 <shouyisheng@gmail.com>
2021-03-10 17:05:49 +0900

    FactoryBotのpasswordを英数字どちらも含むように修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.name}
     email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
     last_name_kanji       {Gimei.last.kanji}
     first_name_kanji      {Gimei.first.kanji}
