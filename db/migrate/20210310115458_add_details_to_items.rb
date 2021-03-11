0f163dc0259521b5afb8b54a7758a62892edbf78
appletea946 <shouyisheng@gmail.com>
2021-03-11 11:04:18 +0900

    itemsテーブルにカラムを追加

---
+++ db/migrate/20210310115458_add_details_to_items.rb
@@ -0,0 +1,13 @@
+class AddDetailsToItems < ActiveRecord::Migration[6.0]
+  def change
+    add_column :items, :name, :string, null: false
+    add_column :items, :description, :string, null: false
+    add_column :items, :category_id, :integer, null: false
+    add_column :items, :status_id, :integer, null: false
+    add_column :items, :shipping_charges_id, :integer, null: false
+    add_column :items, :prefecture_id, :integer, null: false
+    add_column :items, :days_to_ship_id, :integer, null: false
+    add_column :items, :price, :integer, null: false
+    add_reference :items, :user, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_10_111246) do
+ActiveRecord::Schema.define(version: 2021_03_10_115458) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2021_03_10_111246) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "description", null: false
+    t.integer "category_id", null: false
+    t.integer "status_id", null: false
+    t.integer "shipping_charges_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "days_to_ship_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -57,4 +67,5 @@ ActiveRecord::Schema.define(version: 2021_03_10_111246) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "items", "users"
 end
