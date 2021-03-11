1a570359af60d05ac1b4b8e630ad71072dcc006d
appletea946 <shouyisheng@gmail.com>
2021-03-11 14:50:25 +0900

    ストロングパラメーターで保存を許可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,5 +8,17 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render new_item_path
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:description,:category_id,:status_id,:shipping_charges_id,:prefecture_id,:days_to_ship_id,:price,:image).merge(user_id: current_user.id)
   end
 end
