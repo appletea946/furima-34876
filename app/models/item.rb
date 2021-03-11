b845edd0d1c77d79ed3581d7e2a8984841b5eb34
appletea946 <shouyisheng@gmail.com>
2021-03-11 11:06:10 +0900

    各モデルにActiveHashを記述しバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charges
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
+  with_options presence: true do
+    validates :name
+    validates :description
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :status_id
+      validates :shipping_charges_id
+      validates :prefecture_id
+      validates :days_to_ship_id
+    end
+    
+    validates :price
+    validates :image
+  end
 end
