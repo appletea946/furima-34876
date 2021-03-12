0411ecf3eed29354652c3bd5ba477a1b4db39ef4
appletea946 <shouyisheng@gmail.com>
2021-03-12 15:07:29 +0900

    商品編集画面への遷移を出品者のみ許可する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :create, :update]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_root, only: [:edit, :update]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -22,7 +23,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless current_user.id == @item.user_id
   end
 
   def update
@@ -45,5 +45,9 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+  def move_to_root
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
