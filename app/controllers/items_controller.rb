b5d7c2b41fb13786a936d4475ce6946ef87f8b45
appletea946 <shouyisheng@gmail.com>
2021-03-12 14:47:20 +0900

    ログアウト状態時のページ遷移を制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update]
   def index
     @items = Item.order('created_at DESC')
   end
