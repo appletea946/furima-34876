94dd7e22908273a8b5f691d053ee5b29dd41fcda
appletea946 <shouyisheng@gmail.com>
2021-03-15 15:58:07 +0900

    Fakerで値をランダムにする

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :order_address do
-    token             {"tok_abcdefghijk00000000000000"}
-    postal_code       {"000-1111"}
-    prefecture_id     {40}
-    municipality      {"市区町村"}
-    address           {"番地"}
-    phone_number      {00011112222}
-    user_id           {1}
-    item_id           {1}
+    token             { 'tok_abcdefghijk00000000000000' }
+    postal_code       { '000-1111' }
+    prefecture_id     { Faker::Number.between(from: 2, to: 48) }
+    municipality      { Gimei.city.kanji }
+    address           { '番地1-2-3' }
+    phone_number      { Faker::Number.number(digits: 11) }
+    user_id           { 1 }
+    item_id           { 1 }
   end
 end
