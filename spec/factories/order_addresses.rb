709fc59fd5f31b0c8c39090ba55fd2106b0d82a7
appletea946 <shouyisheng@gmail.com>
2021-03-15 19:42:02 +0900

    テストコードとファクトリーボットの修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -2,11 +2,10 @@ FactoryBot.define do
   factory :order_address do
     token             { 'tok_abcdefghijk00000000000000' }
     postal_code       { '000-1111' }
-    prefecture_id     { Faker::Number.between(from: 2, to: 48) }
-    municipality      { Gimei.city.kanji }
+    prefecture_id     { 48 }
+    municipality      { '市区町村' }
     address           { '番地1-2-3' }
-    phone_number      { Faker::Number.number(digits: 11) }
-    user_id           { 1 }
-    item_id           { 1 }
+    building_name     { '建物building' }
+    phone_number      { '09012345678' }
   end
 end
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,11 +3,9 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '商品購入機能' do
     before do
-      @order_address = FactoryBot.build(:order_address)
       @user = FactoryBot.create(:user)
       @item = FactoryBot.create(:item)
-      @order_address.user_id = @user.id
-      @order_address.item_id = @item.id
+      @order_address = FactoryBot.build(:order_address,user_id: @user.id, item_id: @item.id)
       sleep 0.1
     end
 
